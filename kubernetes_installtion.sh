311eb81651b5bfafdadb78a96207cb8226246082
root <root@ip-172-31-36-192.ec2.internal>
2020-03-22 06:49:08 -0400

    kubernetes installation

---
+++ kubernetes_installtion.sh
@@ -0,0 +1,14 @@
+swapoff -a
+yum repolist
+yum install kubeadm docker -y
+systemctl stop firewalld
+systemctl start docker
+systemctl enable docker
+systemctl restart kubelet
+systemctl enable kubelet
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+export kubever=$(kubectl version | base64 | tr -d '\n')
+kubectl get nodes
+
