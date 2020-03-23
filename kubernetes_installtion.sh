82f0879ac9ac52467ba532fbade28fa7db9dc907
vaishnavi deshpande <62397628+20vaishnavi@users.noreply.github.com>
2020-03-23 22:42:17 +0530

    Update kubernetes_installtion.sh

--- kubernetes_installtion.sh
+++ kubernetes_installtion.sh
@@ -12,5 +12,6 @@ mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 export kubever=$(kubectl version | base64 | tr -d '\n')
+kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$kubever"
 kubectl get nodes
 
